be3778abee0c94458fc98cdaf46abda054829158
PacoParra <parra_fj@cantabria.es>
2015-03-23 10:39:35 +0100

    Update td.R
    Realiza el test acumulativo de Durbin

--- td.R
+++ td.R
@@ -1,5 +1,5 @@
 td <- function(y,significance) {
-# Author: Francisco Parra Rodr?guez
+# Author: Francisco Parra Rodriguez
 # Some ideas from:
 #Harvey, A.C. (1978), Linear Regression in the Frequency Domain, International Economic Review, 19, 507-512.
 # DURBIN, J., "Tests for Serial Correlation in Regression Analysis based on the Periodogram ofLeast-Squares Residuals," Biometrika, 56, (No. 1, 1969), 1-15.
@@ -13,10 +13,11 @@ for(i in 2:n) {s1 <-p[i]+s[(i-1)]
 s <- c(s,s1)
 s2 <- s/s[n]
 }
+TestD <- NULL
 data(TestD)
+rm(TestD)
 while (n > 100) n <- 100
-if (significance==1) c<- c(TestD[n,1]) else {if (significance==2) c <- c(TestD[n,2])else {if (significance==3) c <- c(TestD[n,3]) else {if (significance==4) c <- c(TestD[n,4]) 
-c <- c(TestD[n,5])}}}
+c <- subset(TestD, row == n & col==paste("X",as.character(significance),sep=""))$value
 min <- -c+(t/length(p))
 max <- c+(t/length(p))
 data.frame(s2,min,max)
